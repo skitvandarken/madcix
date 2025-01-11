353d29778ecc08adff278b3b3736f95a37f78524
Lukeba Jorge <lukeba.jorge@angolacables.co.ao>
2025-01-11 17:56:04 +0100

    mvp homepage

---
+++ src/app/layout/membros/membros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-membros',
+  imports: [],
+  templateUrl: './membros.component.html',
+  styleUrl: './membros.component.css'
+})
+export class MembrosComponent {
+
+}
--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -4,10 +4,12 @@ import { SliderComponent } from '../../layout/slider/slider.component';
 import { NumerosComponent } from '../../layout/numeros/numeros.component';
 import { ServicosComponent } from '../../layout/servicos/servicos.component';
 import { RodapeComponent } from '../../layout/rodape/rodape.component';
+import { MembrosComponent } from '../../layout/membros/membros.component';
+import { EventosComponent } from "../../layout/eventos/eventos.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [MenuComponent, SliderComponent, NumerosComponent, ServicosComponent, RodapeComponent],
+  imports: [MenuComponent, RodapeComponent, MembrosComponent, SliderComponent, NumerosComponent, ServicosComponent, RodapeComponent, MembrosComponent, EventosComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
