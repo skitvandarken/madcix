d72a697e442e552f2c0f2755f31314c98768f2f6
Pedro Jorge <skitvandarken2013@gmail.com>
2025-02-10 18:12:30 +0100

    membros adicionados

--- src/app/layout/membros/membros.component.ts
+++ src/app/layout/membros/membros.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RodapeComponent } from '../rodape/rodape.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-membros',
-  imports: [],
+  imports: [RodapeComponent, MenuComponent],
   templateUrl: './membros.component.html',
   styleUrl: './membros.component.css'
 })
